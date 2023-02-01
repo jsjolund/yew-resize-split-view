63be5eb48ce8c81c3057219cd329042ccce4fa28
jsjolund <j.sjolund@gmail.com>
2023-02-01 14:49:28 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-mod app;
+mod vertical;
 
-use app::App;
+use vertical::VerticalSplit;
+use yew::{function_component, html, Html};
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let left1 = html! {"left1"};
+    let right1 = html! {"right"};
+
+    let left0 = html! {"left0"};
+    let right0 = html! {
+        <VerticalSplit
+            height={"100vh".to_string()}
+            left={left1}
+            right={right1}
+        />
+    };
+    html! {
+        <VerticalSplit
+            height={"100vh".to_string()}
+            left={left0}
+            right={right0}
+        />
+    }
+}
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
--- src/vertical.rs
+++ src/vertical.rs
@@ -5,31 +5,42 @@ use wasm_bindgen::prelude::Closure;
 use wasm_bindgen::JsCast;
 use web_sys::HtmlElement;
 use yew::prelude::*;
+use yew::virtual_dom::VNode;
 
-#[function_component(App)]
-pub fn app() -> Html {
+#[derive(Clone, Properties, PartialEq)]
+pub struct Props {
+    pub height: String,
+    #[prop_or_default]
+    pub left: VNode,
+    #[prop_or_default]
+    pub right: VNode,
+}
+
+#[function_component]
+pub fn VerticalSplit(props: &Props) -> Html {
     let container = use_node_ref();
-    let resizer = use_node_ref();
-    let left_side = use_node_ref();
+    let drag = use_node_ref();
 
     let is_resizing = use_mut_ref(|| false);
     let x = use_mut_ref(|| 0);
     let y = use_mut_ref(|| 0);
     let left_width = use_mut_ref(|| 50.0);
-    let new_left_width = use_state(|| *left_width.borrow_mut());
     let container_width = use_mut_ref(|| 0);
 
+    let stopped_resizing = use_state(|| false);
+    let new_left_width = use_state(|| *left_width.borrow_mut());
+
     let mut right_style = String::from("flex: 1 1 0%;");
-    let mut left_style = format!("width: {}%;", *new_left_width);
+    let mut left_style = format!("height:{}; width: {}%;", props.height, *new_left_width);
 
     if *is_resizing.borrow_mut() {
-        right_style.push_str("user-select:none; pointer-events:none;  cursor:col-resize;");
-        left_style.push_str("user-select:none; pointer-events:none;  cursor:col-resize;");
+        right_style.push_str("user-select:none; pointer-events:none; cursor:col-resize;");
+        left_style.push_str("user-select:none; pointer-events:none; cursor:col-resize;");
     }
 
     let right_css = Style::new(right_style).expect("Failed to create right style");
     let left_css = Style::new(left_style).expect("Failed to create left style");
-
+    let drag_css = Style::new("cursor:col-resize;").expect("Failed to create drag style");
     {
         // Create window resize listener
         let container_width = container_width.clone();
@@ -38,7 +49,26 @@ pub fn app() -> Html {
             |container| {
                 let div = container
                     .cast::<HtmlElement>()
-                    .expect("resizer not attached to div element");
+                    .expect("drag not attached to div element");
+                let listener = EventListener::new(&window(), "mouseup", move |_| {
+                    *container_width.borrow_mut() = div.get_bounding_client_rect().width() as i32;
+                });
+                move || {
+                    drop(listener);
+                }
+            },
+            container,
+        );
+    }
+    {
+        // Create window resize listener
+        let container_width = container_width.clone();
+        let container = container.clone();
+        use_effect_with_deps(
+            |container| {
+                let div = container
+                    .cast::<HtmlElement>()
+                    .expect("drag not attached to div element");
                 let listener = EventListener::new(&window(), "resize", move |_| {
                     *container_width.borrow_mut() = div.get_bounding_client_rect().width() as i32;
                 });
@@ -53,20 +83,20 @@ pub fn app() -> Html {
         // Create mouse down listener
         let x = x.clone();
         let y = y.clone();
-        let resizer = resizer.clone();
+        let drag = drag.clone();
         let left_width = left_width.clone();
         let is_resizing = is_resizing.clone();
         use_effect_with_deps(
-            |resizer| {
-                let div = resizer
+            |drag| {
+                let div = drag
                     .cast::<HtmlElement>()
-                    .expect("resizer not attached to div element");
-                let left_sidex = div.previous_element_sibling().unwrap();
+                    .expect("drag not attached to div element");
+                let left_side = div.previous_element_sibling().unwrap();
                 let listener =
                     Closure::<dyn Fn(MouseEvent)>::wrap(Box::new(move |ev: MouseEvent| {
                         *x.borrow_mut() = ev.client_x();
                         *y.borrow_mut() = ev.client_y();
-                        *left_width.borrow_mut() = left_sidex.get_bounding_client_rect().width();
+                        *left_width.borrow_mut() = left_side.get_bounding_client_rect().width();
                         *is_resizing.borrow_mut() = true;
                     }));
                 div.add_event_listener_with_callback(
@@ -78,7 +108,7 @@ pub fn app() -> Html {
                     drop(listener);
                 }
             },
-            resizer,
+            drag,
         );
     }
     {
@@ -125,6 +155,7 @@ pub fn app() -> Html {
                     .expect("container not attached to div element");
                 let listener = Closure::<dyn Fn(MouseEvent)>::wrap(Box::new(move |_| {
                     *is_resizing.borrow_mut() = false;
+                    stopped_resizing.set(true);
                 }));
                 div.add_event_listener_with_callback("mouseup", listener.as_ref().unchecked_ref())
                     .unwrap();
@@ -136,14 +167,14 @@ pub fn app() -> Html {
         );
     }
     html! {
-        <main>
         <div ref={container} class="container" style="display: flex;">
-            <div ref={left_side} class={left_css} id="left_panel">{"Left"}</div>
-
-            <div ref={resizer} class="resizer" id="drag"></div>
-
-            <div class={right_css} id="right_panel">{"Right"}</div>
+            <div class={left_css} id="left_panel">
+            { props.left.clone() }
+            </div>
+            <div ref={drag} class={drag_css} id="drag_vertical"></div>
+            <div class={right_css} id="right_panel">
+            { props.right.clone() }
+            </div>
         </div>
-        </main>
     }
 }
